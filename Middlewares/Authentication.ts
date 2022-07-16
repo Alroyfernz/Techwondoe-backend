454252ea3bd5aab9ea9b334bc2aae3c761b92e20
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-07-16 13:13:56 +0530

    fix app crashed

--- Middlewares/Authentication.ts
+++ Middlewares/Authentication.ts
@@ -8,12 +8,12 @@ export default async (req: Request, res: Response, next: NextFunction) => {
 
   try {
     if (!token)
-      res.status(401).json({
+      return res.status(401).json({
         message: "You are not authorized to access this route",
       });
     const user = await jwt.verify(token as string, SECRET_KEY as string);
     if (user) next();
   } catch (error: any) {
-    res.status(403).json(`Error occured due to ${error.message}`);
+    return res.status(403).json(`Error occured due to ${error.message}`);
   }
 };
